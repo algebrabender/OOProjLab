66ebd1f51da483d4646962237d94c48f43cd4fe0
algebrabender <jelena.firulovic@gmail.com>
2021-01-04 15:49:26 +0100

    4.1.2021.

---
+++ LabVezba5/Controllers/IController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabVezba5.Models;
+using LabVezba5.View;
+
+namespace LabVezba5.Controllers
+{
+    public interface IController
+    {
+        void Start();
+        void GameOver();
+        void NewGame();
+        void Draw();
+        //remove ulog?
+    }
+}
---
+++ LabVezba5/Controllers/StandardController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabVezba5.Models;
+using LabVezba5.View;
+
+namespace LabVezba5.Controllers
+{
+    class StandardController : IController
+    {
+        private IModel deck;
+        private IView view;
+
+        public StandardController(IModel deck, IView view)
+        {
+            this.deck = deck;
+            this.view = view;
+            this.view.AddListener(this);
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void GameOver()
+        {
+            
+        }
+
+        public void NewGame()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
